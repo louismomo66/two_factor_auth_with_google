a6838099452f03f8bb36d82b35066a1fc6abbe19
Hassan Bulega <hassanbulega@gmail.com>
2022-10-04 13:58:24 +0300

    perf: use context API and useReducer

---
+++ src/context/appContext.js
@@ -0,0 +1,42 @@
+import React, { useReducer, useContext } from "react";
+import reducer from "./reducer";
+import axios from "axios";
+import { CLEAR_ALERT, DISPLAY_ALERT } from "./actions";
+
+const initialState = {
+  isLoading: false,
+  showAlert: false,
+  alertType: "",
+  alertText: "",
+};
+
+const AppContext = React.createContext();
+
+const AppProvider = ({ children }) => {
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const displayAlert = () => {
+    dispatch({ type: DISPLAY_ALERT });
+    clearAlert();
+  };
+  const clearAlert = () => {
+    setTimeout(() => {
+      dispatch({ type: CLEAR_ALERT });
+    }, 3000);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        ...state,
+        displayAlert,
+        clearAlert,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+const useAppContext = () => {
+  return useContext(AppContext);
+};
+export { AppProvider, initialState, useAppContext };
