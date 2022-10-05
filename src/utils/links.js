38f47f6b251e34b6e64c49fc30eb9d0175a4ae8b
Hassan Bulega <hassanbulega@gmail.com>
2022-10-05 14:47:04 +0300

    chore: add links

---
+++ src/utils/links.js
@@ -0,0 +1,49 @@
+import { IoBarChartSharp } from "react-icons/io5";
+import { MdQueryStats } from "react-icons/md";
+import { FaWpforms } from "react-icons/fa";
+import { ImProfile } from "react-icons/im";
+import {FaHome} from 'react-icons/fa'
+import {MdComputer} from 'react-icons/md'
+import {BsCalendarDayFill} from 'react-icons/bs'
+import {FiBookOpen} from 'react-icons/fi'
+import {BsPersonCheckFill} from 'react-icons/bs'
+const links = [
+  {
+    id: 1,
+    text: "home",
+    path: "/dashboard",
+    icon: <FaHome/>,
+  },
+  {
+    id: 2,
+    text: "labs",
+    path: "/dashboard/labs",
+    icon: <MdComputer />,
+  },
+  {
+    id: 3,
+    text: "reports",
+    path: "/dashboard/reports",
+    icon: <ImProfile />,
+  },
+  {
+    id: 4,
+    text: "schedules",
+    path: "/dashboard/schedules",
+    icon: <BsCalendarDayFill />,
+  },
+  {
+    id: 5,
+    text: "tests",
+    path: "/dashboard/tests",
+    icon: <FiBookOpen />,
+  },
+  {
+    id: 6,
+    text: "account",
+    path: "/dashboard/account",
+    icon: <BsPersonCheckFill />,
+  },
+];
+
+export default links;
