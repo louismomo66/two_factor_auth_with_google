8caca66c9501a05ab738440b546be5de91a2e90e
Hassan Bulega <hassanbulega@gmail.com>
2022-10-05 14:38:40 +0300

    feat: add dashboard components all dashboard components have a shared layout

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Landing, Dashboard, Register, Error } from "./pages";
+import { Landing, Register, Error } from "./pages";
+import {
+  Home,
+  Account,
+  Labs,
+  Reports,
+  SharedLayout,
+  Schedules,
+  Tests,
+} from "./pages/dashboard";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Dashboard />} />
+        <Route path='/dashboard' element={<SharedLayout />}>
+          <Route index path='home' element={<Home />} />
+          <Route path='account' element={<Account />} />
+          <Route path='labs' element={<Labs />} />
+          <Route path='reports' element={<Reports />} />
+          <Route path='tests' element={<Tests />} />
+          <Route path='schedules' element={<Schedules />} />
+        </Route>
+
+        <Route path='/' element={<Landing />} />
         <Route path='/register' element={<Register />} />
-        <Route path='/landing' element={<Landing />} />
         <Route path='*' element={<Error />} />
       </Routes>
     </BrowserRouter>
