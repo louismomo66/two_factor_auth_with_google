1263bb5116926a0b613c131f23ae3e3ff79d2ebc
Hassan Bulega <hassanbulega@gmail.com>
2022-10-14 07:49:17 +0300

    chore: add Profile.js to index.js

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,9 @@
 import SharedLayout from "./SharedLayout";
 import Home from "./Home";
-import Account from "./Account";
+import Profile from "./Profile";
 import Labs from "./Labs";
 import Reports from "./Reports";
 import Schedules from './Schedules'
 import Tests from './Tests'
 
-export { Home, Account, Labs, Reports, SharedLayout,Schedules,Tests };
+export { Home, Profile, Labs, Reports, SharedLayout,Schedules,Tests };
