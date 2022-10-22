29e174e8035d22447476b4e8fcd9a122adbea4a1
Hassan Bulega <hassanbulega@gmail.com>
2022-10-22 21:49:41 +0300

    add syncfusion license

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import "normalize.css";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context/appContext";
+import { registerLicense } from "@syncfusion/ej2-base";
+
+// Registering Syncfusion license key
+registerLicense(
+  "ORg4AjUWIQA/Gnt2VVhjQlFaclhJXGFWfVJpTGpQdk5xdV9DaVZUTWY/P1ZhSXxRd0ViX39bdXZQQGVcVkQ="
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
