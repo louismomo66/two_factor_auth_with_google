fc1ae2b4ff8a671be56020b597ac67a267998ebd
Hassan Bulega <hassanbulega@gmail.com>
2022-11-02 09:27:10 +0300

    add another column to number

--- src/components/labs/LabsList.js
+++ src/components/labs/LabsList.js
@@ -1,12 +1,20 @@
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  GridComponent,
+  Page,
+  Inject,
+  Search,
+  Toolbar,
+} from "@syncfusion/ej2-react-grids";
 
-import { ColumnDirective, ColumnsDirective, GridComponent, Page, Inject, Search, Toolbar } from '@syncfusion/ej2-react-grids';
-import { ButtonComponent } from '@syncfusion/ej2-react-buttons';
-import '../../assets/sf/grid.css';
-import { labService } from '../../services/labs'
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import "../../assets/sf/grid.css";
+import { labService } from "../../services/labs";
 import { AppContext } from "../../context/appContext";
-import React from 'react';
+import React from "react";
 
-const toolbarOptions = ['Search'];
+const toolbarOptions = ["Search"];
 
 export class LabList extends React.Component {
   constructor() {
@@ -16,58 +24,86 @@ export class LabList extends React.Component {
   service = labService(this.context.token);
   grid;
   data;
+
   pageSettings = { pageSize: 10 };
   renderComplete() {
-    if (this.grid && (this.grid.dataSource instanceof Array
-      && !(this.grid.dataSource).length)) {
+    if (
+      this.grid &&
+      this.grid.dataSource instanceof Array &&
+      !this.grid.dataSource.length
+    ) {
       const state = { skip: 0, take: this.pageSettings.pageSize };
       this.dataStateChange(state);
     }
   }
   dataStateChange(state) {
-    console.log("State:", state)
-    const searchTerm = state?.search?.[0]?.key || '';
+    console.log("State:", state);
+    const searchTerm = state?.search?.[0]?.key || "";
     let page = 1;
     let take = state.take || 1;
-    const skip = state.skip || 0
-    if(state.skip > 0)
-      page = 1 + skip / take;
+    const skip = state.skip || 0;
+    if (state.skip > 0) page = 1 + skip / take;
     this.service.getPage(page, take, searchTerm).then((gridData) => {
       if (this.grid) {
-        this.grid.dataSource = gridData
+        this.grid.dataSource = gridData;
       }
     });
   }
 
   gridTemplate(props) {
     const id = props.id;
-    return (<div className='text-danger'>
-      <ButtonComponent onClick={() => console.log('clicked:', id)} cssClass='e-info'>Request</ButtonComponent>
-    </div>);
+    return (
+      <div className='text-danger'>
+        <ButtonComponent
+          onClick={() => console.log("clicked:", id)}
+          cssClass='e-info'
+        >
+          Request
+        </ButtonComponent>
+      </div>
+    );
   }
-  static contextType = AppContext
+
+  static contextType = AppContext;
+
   render() {
     this.renderComplete = this.renderComplete.bind(this);
     this.dataStateChange = this.dataStateChange.bind(this);
     return (
       <div className='control-pane'>
         <div className='control-section'>
-          <GridComponent dataSource={this.data} ref={g => this.grid = g} pageSettings={this.pageSettings}
-            dataBound={this.renderComplete} dataStateChange={this.dataStateChange} allowPaging={true}
-            toolbar={toolbarOptions}>
+          <GridComponent
+            dataSource={this.data}
+            ref={(g) => (this.grid = g)}
+            pageSettings={this.pageSettings}
+            dataBound={this.renderComplete}
+            dataStateChange={this.dataStateChange}
+            allowPaging={true}
+            toolbar={toolbarOptions}
+          >
             <ColumnsDirective>
-              <ColumnDirective field='name' headerText="Lab Name" width='130' />
-              <ColumnDirective field='code' headerText='Lab Code' width='50' />
-              {/* <ColumnDirective field='startDate' headerText='Starts' width='70'  /> */}
-              <ColumnDirective field='StopDate' headerText='Ends' width='70'  />
-              <ColumnDirective headerText='Request Access' width='60' template={this.template}  />
+              <ColumnDirective headerText='No' width='20' textAlign='Right' />
+              <ColumnDirective field='name' headerText='Lab Name' width='100' />
+              <ColumnDirective field='code' headerText='Lab Code' width='30' />
+              <ColumnDirective
+                field='startDate'
+                headerText='Starts'
+                width='70'
+              />
+
+              {/* <ColumnDirective field='StopDate' headerText='Ends' width='70' /> */}
+              <ColumnDirective
+                headerText='Request Access'
+                width='60'
+                template={this.template}
+              />
             </ColumnsDirective>
             <Inject services={[Page, Search, Toolbar]} />
           </GridComponent>
         </div>
-      </div>)
+      </div>
+    );
   }
-};
-
+}
 
 export default LabList;
