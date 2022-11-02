f6b5cac75d59e06d00c0408312e7d5cc3aa1d6dc
Hassan Bulega <hassanbulega@gmail.com>
2022-11-02 09:25:49 +0300

    set maxDate

--- src/components/Scheduler.js
+++ src/components/Scheduler.js
@@ -6,64 +6,22 @@ import {
   Inject,
   ViewsDirective, ViewDirective
 } from "@syncfusion/ej2-react-schedule";
+import { extend } from "@syncfusion/ej2-base";
 // import { DataManager, ODataV4Adaptor } from "@syncfusion/ej2-data";
 import { DataManager, WebApiAdaptor } from "@syncfusion/ej2-data";
-// import labData from "../utils/labData";
+import labData from "../utils/labData";
 import "../assets/scheduler.css";
 
 class Scheduler2 extends React.Component {
-  localData = {
-    dataSource: [
-      {
-        Id: 1,
-        Subject: "Half Wave Rectification",
-        StartTime: new Date(2022, 10, 15, 9, 30),
-        EndTime: new Date(2022, 10, 10, 11, 0),
-        // IsBlock:true,
-        IsReadonly: true,
-        IsAllDay: true,
-      },
-      {
-        Id: 2,
-        Subject: "Full Wave Rectification",
-        StartTime: new Date(2022, 9, 23, 12, 0),
-        EndTime: new Date(2022, 9, 24, 14, 0),
-        Description: "electronics na mutima",
-        Status: "Completed",
-
-        // IsBlock:true,
-        IsReadonly: true,
-      },
-      {
-        Id: 3,
-        Subject: "Half Wave Rectification",
-        StartTime: new Date(2022, 9, 18, 0, 0),
-        EndTime: new Date(2022, 9, 21, 24, 0),
-        IsBlock: true, // block appoitment for specified time
-        IsReadonly: true, // disables delete/edit
-        IsAllDay: true,
-      },
-      {
-        Id: 4,
-        Subject: "Full Wave Rectification",
-        StartTime: new Date(2022, 9, 25, 22, 0),
-        EndTime: new Date(2022, 9, 25, 23, 30),
-        // IsBlock:true,
-        IsReadonly: true,
-        // RecurrenceRule: "FREQ=DAILY;INTERVAL=2;COUNT=10",
-      },
-      {
-        Id: 5,
-        Subject: "Digital electronics",
-        StartTime: new Date(2022, 9, 20, 12, 0),
-        EndTime: new Date(2022, 9, 20, 15, 30),
-        // IsBlock:true,
-        IsReadonly: true,
-        RecurrenceRule: "FREQ=DAILY;INTERVAL=3;COUNT=5",
-      },
-    ],
-    // labData
-  };
+  constructor() {
+    super(...arguments);
+    this.data = extend([],labData,null,true);
+  }
+  onEventRendered(args) {
+    if (args.data.EndTime < this.scheduleObj.selectedDate) {
+      args.element.classList.add("e-past-app");
+    }
+  }
   remoteData = new DataManager({
     url: "https://ej2services.syncfusion.com/production/web-services/api/Schedule",
     adaptor: new WebApiAdaptor(),
@@ -71,28 +29,38 @@ class Scheduler2 extends React.Component {
   });
 
   onActionBegin(args) {
-    console.log('Event args:', args)
-    if (args.requestType === 'eventCreate' && args.data.length > 0) {
+    console.log("Event args:", args);
+    if (args.requestType === "eventCreate" && args.data.length > 0) {
       let eventData = args.data[0];
       let eventField = this.scheduleObj.eventFields;
       let startDate = eventData[eventField.startTime];
       let endDate = eventData[eventField.endTime];
-      console.log("Start:", startDate, "End:", endDate)
+      console.log("Start:", startDate, "End:", endDate);
       args.cancel = !this.scheduleObj.isSlotAvailable(startDate, endDate);
     }
   }
 
   render() {
-    var date = new Date()
+    var date = new Date();
     var yesterday = new Date(date.getTime());
     yesterday.setDate(date.getDate() - 1);
+    let maxdate = new Date(date.getTime());
+    maxdate.setDate(date.getDate() + 7);
     return (
-      <ScheduleComponent currentView='Week' eventSettings={this.localData} minDate={yesterday} allowMultiCellSelection={false}
-        actionBegin={this.onActionBegin.bind(this)}  >
+      <ScheduleComponent
+        currentView='Week'
+        eventSettings={{ dataSource: this.data }}
+        minDate={yesterday}
+        maxDate={maxdate}
+        allowMultiCellSelection={false}
+        actionBegin={this.onActionBegin.bind(this)}
+        ref={(schedule) => (this.scheduleObj = schedule)}
+        eventRendered={this.onEventRendered.bind(this)}
+      >
         <ViewsDirective>
           <ViewDirective option='Week' />
         </ViewsDirective>
-        <Inject services={[ Week]} />
+        <Inject services={[Week]} />
       </ScheduleComponent>
     );
   }
