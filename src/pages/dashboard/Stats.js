3f2597e58de76c3b0303a227e749a1be61558a7c
Hassan Bulega <hassanbulega@gmail.com>
2022-11-30 09:11:57 +0300

    fix: add dispatch to dependency array of useEffect

--- src/pages/dashboard/Stats.js
+++ src/pages/dashboard/Stats.js
@@ -1,18 +1,17 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { displayStats } from "../../store/actions/labActions";
 import { StatsContainer, Loading, ChartsContainer } from "../../components";
 
 const Stats = () => {
   const { monthlyLabAccesses } = useSelector((state) => state.lab);
-  const [loading, setLoading] = useState(false);
+
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(displayStats());
-  }, []);
+  }, [dispatch]);
 
-  
   return (
     <>
       {/* <StatsContainer />
